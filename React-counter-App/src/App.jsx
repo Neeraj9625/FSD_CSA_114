eda49cd0bb7c981ebb445716db63519a8283a691
neeraj <kumarneeraj18769@gmail.com>
2024-12-04 14:28:22 +0530

    using routes

--- React-counter-App/src/App.jsx
+++ React-counter-App/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes,Route} from "react-router-dom"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +8,11 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path = "/"></Route>
+        <Route path = "/" element = {<h1>Home Page</h1>}></Route>
+        <Route path = "/counter" element = {<h1>counter Page</h1>}></Route>
+        <Route path = "/stopwatch" element = {<h1>stop watch</h1>}></Route>
+        <Route path = "*" element = {<h1>No  Page Available</h1>}></Route>
+
       </Routes>
     </div>
   )
